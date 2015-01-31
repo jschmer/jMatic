33e2dab8b2c07ddbcfcca283c6e6e6f550d2319d
Jens Schmer <der.liggers@gmail.com>
2015-01-31 12:07:18 +0100

    remove experimental, non-working minify script

--- minify.bat
+++
@@ -1,4 +0,0 @@
-REM requires nodejs uglifyjs module!
-REM npm install uglify-js -g
-
-start uglifyjs js\jsExtensions.js js\lang.js js\common.js js\XMLparsing.js js\data.js js\jMaticController.js js\compiled_templates.js js\userdefined_groups.js -o jMatic.min.js --source-map jMatic.min.js.map -p relative -c -m
